7c1a90a787da501c47b71dc8ea9702817cf4dd8d
wenhan <133662856+ZZMoon12@users.noreply.github.com>
2025-11-16 10:36:37 -0800

    Add files via upload

---
+++ Assets/Settings/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject settingsPanel;
+
+    public void StartGame()
+    {
+        // ⚠️ 把 "GameScene" 改成你主游戏场景的名字
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
+
